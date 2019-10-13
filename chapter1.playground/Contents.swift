c3055b419fb2986d34f327a54180bf2e18054258
Steve Morris <steve@ajn.in>
2019-10-13 11:02:15 -0700

    chapter1 better section titles

--- chapter1.playground/Contents.swift
+++ chapter1.playground/Contents.swift
@@ -30,6 +30,9 @@
 
 import Foundation
 
+// Chapter 1: Expressions, Variables & Constants
+
+// Code comments
 // This is a comment. It is not executed.
 
 // This is also a comment.
@@ -50,15 +53,14 @@ import Foundation
  Back to the first.
  */
 
-// Print string to debug area/console.
+// Printing out
 print("Hello, Swift Apprentice reader!")
 
-// Simple expressions using operators.
+// Simple operations
 2 + 6
 10 - 2
 2 * 4
 24 / 3
-
 // Removing the whitespace is an all or nothing, you can’t mix styles.
 2+6   // OK
 2 + 6 // OK
@@ -69,15 +71,15 @@ print("Hello, Swift Apprentice reader!")
 22 / 7 // Result is only a whole number/integer, not a decimal.
 22.0 / 7.0 // Result will now be a floating point number/decimal.
 
-// Remainder operator
+// The remainder operation
 28 % 10
 (28.0).truncatingRemainder(dividingBy: 10.0) // same result using decimal numbers.
 
-// Shift operators
+// Shift operations
 1 << 3
 32 >> 2
 
-// Order of operations, operation precedence
+// Order of operations
 ((8000 / (5 * 10)) - 32) >> (29 % 5)
 350 / 5 + 2
 350 / (5 + 2)
@@ -100,6 +102,7 @@ let pi: Double = 3.14159
 var variableNumber: Int = 42
 variableNumber = 0
 variableNumber = 1_000_000
+// Using meaningful names
 var 🐶💩: Int = -1
 
 // Increment and decrement
@@ -115,11 +118,15 @@ counter *= 3 // same as counter = counter * 3
 counter /= 2 // same as counter = counter / 2
 
 // Mini-exercises
+// Exercise 1
 let myAge: Int = 33
+// Exercise 2
 var averageAge: Double = 33
 averageAge = (averageAge + 30) / 2
+// Exercise 3
 let testNumber: Int = 89897
 let evenOdd = testNumber % 2 // result is 1 for odd testNumbers and 0 for evens
+// Exercise 4
 var answer: Int = 0
 answer += 1
 answer += 10
