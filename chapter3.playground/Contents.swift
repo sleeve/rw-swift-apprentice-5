018f73a3c7573d7bcc63a0be8e0a436329e40b7e
Steve Morris <steve@ajn.in>
2019-10-13 10:43:11 -0700

    chapter 3 work

--- chapter3.playground/Contents.swift
+++ chapter3.playground/Contents.swift
@@ -1,3 +1,137 @@
+/**
+* Copyright (c) 2019 Razeware LLC
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+* distribute, sublicense, create a derivative work, and/or sell copies of the
+* Software in any work that is designed, intended, or marketed for pedagogical or
+* instructional purposes related to programming, coding, application development,
+* or information technology.  Permission for such use, copying, modification,
+* merger, publication, distribution, sublicensing, creation of derivative works,
+* or sale is expressly withheld.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
+*/
+
 import Foundation
 
-var str = "Hello, playground"
+// Chapter 3: Basic Control Flow
+
+// Comparison operators
+let yes: Bool = true
+let no: Bool = false
+
+let yes2 = true
+let no2 = false
+
+// Boolean operators
+let doesOneEqualTwo = (1 == 2)
+let doesOneNotEqualTwo = (1 != 2)
+let alsoTrue = !(1 == 2)
+let isOneGreaterThanTwo = (1 > 2)
+let isOneLessThanTwo = (1 < 2)
+let and = true && true
+let or = true || false
+let andTrue = 1 < 2 && 4 > 3
+let andFalse = 1 < 2 && 3 > 4
+let orTrue = 1 < 2 || 3 > 4
+let orFalse = 1 == 2 || 3 == 4
+let andOr = (1 < 2 && 3 > 4) || 1 < 4
+
+// String equality
+let guess = "dog"
+let dogEqualsCat = guess == "cat"
+let order = "cat" < "dog"
+
+// Toggling a Bool
+var switchState = true
+switchState.toggle() // switchState = false
+switchState.toggle() // switchState = true
+
+// Mini-exercises
+// Exercise 1
+let myAge: Int = 31
+let isTeenager: Bool = myAge >= 13 && myAge <= 19
+
+// Exercise 2
+let theirAge: Int = 30
+let bothTeenagers: Bool = theirAge >= 13 && theirAge <= 19 && isTeenager
+
+// Exercise 3
+let reader: String = "Steve Morris"
+let author: String = "Matt Galloway"
+let authorIsReader: Bool = reader == author
+
+// Exercise 4
+let readerBeforeAuthor: Bool = reader < author
+
+// The if statement
+if 2 > 1 {
+    print("Yes, 2 is greater than 1.")
+}
+
+let animal = "Fox"
+if animal == "Cat" || animal == "Dog" {
+    print("Animal is a house pet.")
+} else {
+    print("Animal is not a house pet.")
+}
+
+let hourOfDay = 12
+var timeOfDay = ""
+if hourOfDay < 6 {
+    timeOfDay = "Early morning"
+} else if hourOfDay < 12 {
+    timeOfDay = "Morning"
+} else if hourOfDay < 17 {
+    timeOfDay = "Afternoon"
+} else if hourOfDay < 20 {
+    timeOfDay = "Evening"
+} else if hourOfDay < 24 {
+    timeOfDay = "Late evening"
+} else {
+    timeOfDay = "INVALID HOUR!"
+}
+print(timeOfDay)
+
+// Short circuiting
+let name: String = "Steve Morris"
+
+if 1 > 2 && name == "Matt Galloway" {
+    // ...
+}
+
+if 1 < 2 || name == "Matt Galloway" {
+    // ...
+}
+
+// Encapsulating variables
+var hoursWorked = 45
+var price = 0
+if hoursWorked > 40 {
+    let hoursOver40 = hoursWorked - 40
+    price += hoursOver40 * 50
+    hoursWorked -= hoursOver40
+}
+price += hoursWorked * 25
+print(price)
+//print(hoursOver40) // constant is declared within the if statement so it can't be accessed outside of its scope
+
+// The ternary conditional operator
+
+
