d2fdcedc8f4966fab6baec6b6f8e7a76b358d6ba
Steve Morris <steve@ajn.in>
2019-10-14 16:31:46 -0700

    some of chapter3 challenges

--- chapter3.playground/Contents.swift
+++ chapter3.playground/Contents.swift
@@ -133,5 +133,129 @@ print(price)
 //print(hoursOver40) // constant is declared within the if statement so it can't be accessed outside of its scope
 
 // The ternary conditional operator
+let a = 5
+let b = 10
 
+let min: Int
+if a < b {
+    min = a
+} else {
+    min = b
+}
+
+let max: Int
+if a > b {
+    max = a
+} else {
+    max = b
+}
+// (<CONDITION>) ? <TRUE VALUE> : <FALSE VALUE>
+let c = 5
+let d = 10
+let min2 = c < d ? a : b
+let max2 = c > d ? a : b
+
+// Mini-exercises
+// Exercise 1
+let myAge2: Int = 18
+if myAge2 >= 13 && myAge2 <= 19 {
+    print("Teenager")
+} else {
+    print("Not a teenager")
+}
+
+// Exercise 2
+let answer = myAge2 >= 13 && myAge2 <= 19 ? "Teenager" : "Not a teenager"
+print(answer)
+
+// While loops
+var sum = 1
+while sum < 1000 {
+    sum = sum + (sum + 1)
+}
+print(sum)
+
+// Repeat-while loops
+sum = 1
+repeat {
+    sum = sum + (sum + 1)
+} while sum < 1000
+print(sum)
+
+sum = 1
+while sum < 1 { // the condition is already false so the body won't run
+    sum = sum + (sum + 1)
+}
+print(sum)
+
+sum = 1
+repeat {
+    sum = sum + (sum + 1)
+} while sum < 1 // the loop will execute once so sum should be 3
+print(sum)
+
+// Breaking out of a loop
+sum = 1
+while true {
+    sum = sum + (sum + 1)
+    if sum >= 1000 {
+        break
+    }
+}
+print(sum)
+
+// Mini-exercises
+// Exercise 1
+var counter = 0
+while counter < 10 {
+    print("counter is \(counter)")
+    counter += 1
+}
+print(counter)
+
+// Exercise 2
+counter = 0
+var roll = 0
+repeat {
+    roll = Int.random(in: 0...5)
+    counter += 1
+    print("After \(counter) rolls, roll is \(roll)")
+} while roll != 0
+
+// Challenges
+// Challenge 1: Find the error
+let firstName = "Matt"
+if firstName == "Matt" {
+    let lastName = "Galloway"
+} else if firstName == "Ray" {
+    let lastName = "Wenderlich"
+}
+//let fullName = firstName + " " + lastName // can't reference lastName as it was created in the if loop scope
+
+// Challenge 2: Boolean challenge
+let answer1 = true && true // answer1 = true
+let answer2 = false || false // answer2 = false
+let answer3 = (true && 1 != 2) || (4 > 3 && 100 < 1) // answer3 = true
+let answer4 = ((10 / 2) > 3) && ((10 % 2) == 0) //answer4 = true
+
+// Challenge 3: Snakes and ladders
+let currentPosition = 1
+let diceRoll = 5
+var nextPosition = currentPosition + diceRoll
+if nextPosition == 3 {
+    nextPosition = 15
+} else if nextPosition == 7 {
+    nextPosition = 12
+} else if nextPosition == 11 {
+    nextPosition = 2
+} else if nextPosition == 17 {
+    nextPosition = 9
+}
+print(nextPosition)
+
+// Challenge 4: Number of days in a month
+let month: String = "October"
+let year: Int = 2019
+var numberOfDays: Int
+if month == "January"
 
