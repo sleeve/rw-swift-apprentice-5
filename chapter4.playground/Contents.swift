cc997b720caf8d32d9952624c2c402e9a43f7dc7
Steve Morris <steve@ajn.in>
2019-10-20 10:34:43 -0700

    completed main chapter4

--- chapter4.playground/Contents.swift
+++ chapter4.playground/Contents.swift
@@ -181,3 +181,92 @@ default:
 }
 
 // Partial matching
+let coordinates = (x: 3, y: 2, z: 5)
+switch coordinates {
+case (0, 0, 0): // 1
+    print("Origin")
+case (_, 0, 0): // 2
+    print("On the x-axis.")
+case (0, _, 0): // 3
+    print("On the y-axis.")
+case (0, 0, _): // 4
+    print("On the z-axis.")
+default: // 5
+    print("Somewhere in space")
+}
+
+switch coordinates {
+case (0, 0, 0):
+    print("Origin")
+case (let x, 0, 0):
+    print("On the x-axis at x = \(x)")
+case (0, let y, 0):
+    print("On the y-axis at y = \(y).")
+case (0, 0, let z):
+    print("On the z-axis at z = \(z)")
+case let (x, y, z):
+    print("Somewhere in space at x = \(x), y = \(y), z = \(z)")
+}
+
+switch coordinates {
+case let (x, y, _) where y == x:
+    print("Along the y = x line.")
+case let (x, y, _) where y == x * x:
+    print("Along the y = x^2 line.")
+default:
+    break
+}
+
+// Mini-exercises
+// Exercise 1
+let age: Int = 63
+
+switch age {
+case 0...2:
+    print("Infant")
+case 3...12:
+    print("Child")
+case 13...19:
+    print("Teenager")
+case 20...39:
+    print("Adult")
+case 40...60:
+    print("Middle aged")
+case let x where x >= 61:
+    print("Elderly")
+default:
+    fatalError()
+}
+
+// Exercise 2
+let person: (name: String, age2: Int) = ("Steve", 31)
+
+switch person {
+case (let name, 0...2):
+    print("\(name) is an infant.")
+case (let name, 3...12):
+    print("\(name) is a child.")
+case (let name, 13...19):
+    print("\(name) is a teenager.")
+case (let name, 20...39):
+    print("\(name) is an adult.")
+case (let name, 40...60):
+    print("\(name) is middle aged.")
+case let (name, age2) where age2 >= 61:
+    print("\(name) is elderly.")
+default:
+    fatalError()
+}
+
+// Challenges
+// Challenge 1: How many times
+
+// Challenge 2: Count the letter
+
+// Challenge 3: What will print
+
+// Challenge 4: Closed range size
+
+// Challenge 5: The final countdown
+
+// Challenge 6: Print a sequence
